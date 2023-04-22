7fa85ff62fe29a6253f0c46b50d1d200d19b0f8d
imabhish <imabhish143@gmail.com>
2023-04-22 22:51:29 +0530

    first commit

---
+++ betime/src/api/conn.php
@@ -0,0 +1,21 @@
+<?php
+/*
+This file contains database config.phpuration assuming you are running mysql using user "root" and password ""
+*/
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'betimeli_db');
+define('DB_PASSWORD', 'GNZubfF[6sxe');
+define('DB_NAME', 'betimeli_dbtrova');
+
+
+// Try connecting to the Database
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+//Check the connection
+if($conn == false){
+    dir('Error: Cannot connect');
+    Echo"Fail";
+}
+
+?>
